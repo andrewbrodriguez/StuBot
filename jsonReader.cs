32a7a5f2b9346962fcc908daa3423279288bfb6f
Andrew Rodriguez <andrewbrodriguez@gmail.com>
2023-02-21 14:50:44 -0500

    c# script for json reading

---
+++ jsonReader.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jsonReader : MonoBehaviour
+{
+
+    public TextAsset textJSON;
+
+    [System.Serializable]
+    public class User
+    {
+        public string user;
+        public int xpos;
+        public int ypos;
+        public string emotion;
+
+    }
+
+    [System.Serializable]
+    public class userList 
+    {
+        public User[] Users;
+    }
+
+    public userList myUsers = new userList();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myUsers = JsonUtility.FromJson<userList>(textJSON.text); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+   

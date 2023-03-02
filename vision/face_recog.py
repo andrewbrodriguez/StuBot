a823e7b117121637b01fe13a2bf6ee2519226299
JRyanShue <ryan_shue23@milton.edu>
2023-03-02 15:17:16 -0500

    conda, argparse, docs, refactor

--- vision/face_recog.py
+++ vision/face_recog.py
@@ -1,18 +1,46 @@
+
+'''
+Run facial recognition on a given image (pre-cropped using a bounding box). 
+Specify flags for the different operations we want to perform in a loop. 
+
+'''
+
+
 import cv2
 from deepface import DeepFace
+import argparse
 import json
 import pandas as pd
 import numpy as np
 
-to_analyze = cv2.imread("Test.png")
 
-analysis = DeepFace.analyze(to_analyze, enforce_detection = False)
 
+# TODO(Ryan or Yaman): make this work with the actual formula
 def EMAUpdate(vector):
     return new_vector
 
 
-json_object = json.dumps(analysis['emotion'], indent=4)
+def main():
 
-with open("output.json", "w") as file:
-    file.write(json_object)
+    parser = argparse.ArgumentParser(description='Run facial recognition on an image.')
+    parser.add_argument('--emotion', action='store_true')
+    args = parser.parse_args()
+
+    to_analyze = cv2.imread('assets/Test.jpg')
+
+    # Dictionary
+    json_object = {}
+
+    if args.emotion:
+        analysis = DeepFace.analyze(to_analyze, enforce_detection = False)[0]
+        json_object['emotion'] = analysis['emotion']
+
+    with open('out/output.json', 'w') as file:
+        file.write(json.dumps(json_object, indent=4))
+
+
+
+if __name__ == "__main__":
+    main()
+
+
